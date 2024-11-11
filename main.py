b184c8e92a99b0ed1e17776728ee536586ed38a1
Anastsia0kuzya <nastia.2006.10@mail.ru>
2024-11-11 14:18:23 +0100

    guassian_final

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 import linecache
 import re
+atomic_number = {"H":[1,[255,255,255],"FFFFFF"],"D":[1,[255,255,192],"FFFFC0"],"T":[1,[255,255,160],"FFFFA0"],
+                 "He":[2,[217,255,255],"D9FFFF"],"Li":[3,[204,128,255],"CC80FF"],"Be":[4,[194,255,0],"C2FF00"],
+                 "B":[5,[255,181,181],"FFB5B5"],"C":[6,[144,144,144],"909090"],"N":[7,[48,80,248],"3050F8"],
+                 "O":[8,[255,13,13],"FF0D0D"],"F":[9,[144,224,80],"90E050"],"Ne":[10,[179,227,245],"B3E3F5"],
+                 "Na":[11,[171,92,242],"AB5CF2"],"Mg":[12,[138,255,0],"8AFF00"],"Al":[13,[191,166,166],"BFA6A6"],
+                 "Si":[14,[240,200,160],"F0C8A0"],"P":[15,[255,128,0],"FF8000"],"S":[16,[255,255,48],"FFFF30"],
+                 "Cl":[17,[31,240,31],"1FF01F"],"Ar":[18,[128,209,227],"80D1E3"],"K":[19,[143,64,212],"8F40D4"],
+                 "Ca":[20,[61,255,0],"3DFF00"],"Sc":[21,[230,230,230],"E6E6E6"],"Ti":[22,[191,194,199],"BFC2C7"],
+                 "V":[23,[166,166,171],"A6A6AB"],"Cr":[24,[138,153,199],"8A99C7"],"Mn":[25,[156,122,199],"9C7AC7"],
+                 "Fe":[26,[224,102,51],"E06633"],"Co":[27,[240,144,160],"F090A0"],"Ni":[28,[80,208,80],"50D050"],
+                 "Cu":[29,[200,128,51],"C88033"],"Zn":[30,[125,128,176],"7D80B0"],"Ga":[31,[194,143,143],"C28F8F"],
+                 "Ge":[32,[102,143,143],"668F8F"],"As":[33,[189,128,227],"BD80E3"],"Se":[34,[255,161,0],"FFA100"],
+                 "Br":[35,[166,41,41],"A62929"],"Kr":[36,[92,184,209],"5CB8D1"],"Rb":[37,[112,46,176],"702EB0"],
+                 "Sr":[38,[0,255,0],"00FF00"],"Y":[39,[148,255,255],"94FFFF"],"Zr":[40,[148,224,224],"94E0E0"],
+                 "Nb":[41,[115,194,201],"73C2C9"],"Mo":[42,[84,181,181],"54B5B5"],"Tc":[43,[59,158,158],"3B9E9E"],
+                 "Ru":[44,[36,143,143],"248F8F"],"Rh":[45,[10,125,140],"0A7D8C"],"Pd":[46,[0,105,133],"006985"],
+                 "Ag":[47,[192,192,192],"C0C0C0"],"Cd":[48,[255,217,143],"FFD98F"],"In":[49,[166,117,115],"A67573"],
+                 "Sn":[50,[102,128,128],"668080"],"Sb":[51,[158,99,181],"9E63B5"],"Te":[52,[212,122,0],"D47A00"],
+                 "I":[53,[148,0,148],"940094"],"Xe":[54,[66,158,176],"429EB0"],"Cs":[55,[87,23,143],"57178F"],
+                 "Ba":[56,[0,201,0],"00C900"],"La":[57,[112,212,255],"70D4FF"],"Ce":[58,[255,255,199],"FFFFC7"],
+                 "Pr":[59,[217,255,199],"D9FFC7"],"Nd":[60,[199,255,199],"C7FFC7"],"Pm":[61,[163,255,199],"A3FFC7"],
+                 "Sm":[62,[143,255,199],"8FFFC7"],"Eu":[63,[97,255,199],"61FFC7"],"Gd":[64,[69,255,199],"45FFC7"],
+                 "Tb":[65,[48,255,199],"30FFC7"],"Dy":[66,[31,255,199],"1FFFC7"],"Ho":[67,[0,255,156],"00FF9C"],
+                 "Er":[68,[0,230,117],"00E675"],"Tm":[69,[0,212,82],"00D452"],"Yb":[70,[0,191,56],"00BF38"],
+                 "Lu":[71,[0,171,36],"00AB24"],"Hf":[72,[77,194,255],"4DC2FF"],"Ta":[73,[77,166,255],"4DA6FF"],
+                 "W":[74,[33,148,214],"2194D6"],"Re":[75,[38,125,171],"267DAB"],"Os":[76,[38,102,150],"266696"],
+                 "Ir":[77,[23,84,135],"175487"],"Pt":[78,[208,208,224],"D0D0E0"],"Au":[79,[255,209,35],"FFD123"],
+                 "Hg":[80,[184,184,208],"B8B8D0"],"Tl":[81,[166,84,77],"A6544D"],"Pb":[82,[87,89,97],"575961"],
+                 "Bi":[83,[158,79,181],"9E4FB5"],"Po":[84,[171,92,0],"AB5C00"],"At":[85,[117,79,69],"754F45"],
+                 "Rn":[86,[66,130,150],"428296"],"Fr":[87,[66,0,102],"420066"],"Ra":[88,[0,125,0],"007D00"],
+                 "Ac":[89,[112,171,250],"70ABFA"],"Th":[90,[0,186,255],"00BAFF"],"Pa":[91,[0,161,255],"00A1FF"],
+                 "U":[92,[0,143,255],"008FFF"],"Np":[93,[0,128,255],"0080FF"],"Pu":[94,[0,107,255],"006BFF"],
+                 "Am":[95,[84,92,242],"545CF2"],"Cm":[96,[120,92,227],"785CE3"],"Bk":[97,[138,79,227],"8A4FE3"],
+                 "Cf":[98,[161,54,212],"A136D4"],"Es":[99,[179,31,212],"B31FD4"],"Fm":[100,[179,31,186],"B31FBA"],
+                 "Md":[101,[179,13,166],"B30DA6"],"No":[102,[189,13,135],"BD0D87"],"Lr":[103,[199,0,102],"C70066"],
+                 "Rf":[104,[204,0,89],"CC0059"],"Db":[105,[209,0,79],"D1004F"],"Sg":[106,[217,0,69],"D90045"],
+                 "Bh":[107,[224,0,56],"E00038"],"Hs":[108,[230,0,46],"E6002E"],"Mt":[109,[235,0,38],"EB0026"],
+                 "Ds":[110,[0,0,0],"000000"],"Rg":[111,[0,0,0],"000000"],"Cn":[112,[0,0,0],"000000"],"Nh":[113,[0,0,0],"000000"],
+                 "Fl":[114,[0,0,0],"000000"],"Mc":[115,[0,0,0],"000000"],"Lv":[116,[0,0,0],"000000"],"Ts":[117,[0,0,0],"000000"],
+                 "Og":[118,[0,0,0],"000000"],"UND": [999,[0,0,0],"000000"]}
+
 def energy_ORCA(orca_opt_nohup):
 # TODO:выводит список энергий каждого большого и маленького шага
     total_energy = 'Total Energy'
@@ -81,6 +122,15 @@ def energy_ORCA(orca_opt_nohup):
     return list_total_energy_def
 
 
+def symbol_to_atomic(list_values):
+    #TODO: преобразует симолы эллементов в атомные нормера (подхидт только для тройного списка, где элемент на 0 позиции)
+    for s in list_values:
+        for k in s:
+            if k[0] in atomic_number:
+                k[0] = atomic_number[k[0]][0];
+    return list_values
+
+
 def coords_ORCA(orca_opt_nohup):
     #TODO: выводит список координат молекулы по каджому большому шагу
     '''Список списков координат'''
@@ -120,6 +170,9 @@ def coords_ORCA(orca_opt_nohup):
     #разбиваем координаты на списки
     for i in range(len(list_CARTESIAN_COORDINATES)):
         list_CARTESIAN_COORDINATES[i] = [s.split() for s in list_CARTESIAN_COORDINATES[i]]
+    #print(list_CARTESIAN_COORDINATES)
+    if list_CARTESIAN_COORDINATES[0][0][0] is not int:
+        list_CARTESIAN_COORDINATES = symbol_to_atomic(list_CARTESIAN_COORDINATES)
     final_coord = list_CARTESIAN_COORDINATES[5]
     return list_CARTESIAN_COORDINATES, total_time, final_coord
 
@@ -223,8 +276,8 @@ def energy_GAUSSIAN(guassian_opt_nohup):
         for t in range(len(i)):
             val = float(i[t].split()[1])
             i[t] = val
-    print(len(list_total_energy_fin))
-    return list_total_energy_fin
+    final_energy = list_total_energy_fin[-1][-1]
+    return list_total_energy_fin, final_energy
 
 def coords_GAUSSIAN(guassian_opt_nohup):
     start_line_coord = []
@@ -236,6 +289,8 @@ def coords_GAUSSIAN(guassian_opt_nohup):
                 start_line_coord.append(index)
             if ' Rotational constants ' in k:
                 end_line_coord.append(index)
+            if ' Job cpu time:  ' in k:
+                total_time_job = k[19:60]
     with open(guassian_opt_nohup, 'r') as file:
         lines = file.readlines()
         coord = []
@@ -262,15 +317,40 @@ def coords_GAUSSIAN(guassian_opt_nohup):
     #делаем координаты не строками
     for i in list_COORDINATES:
         for k in range(len(i)):
+            i[k][0] = int(i[k][0])
             i[k][1] = float(i[k][1])
             i[k][2] = float(i[k][2])
             i[k][3] = float(i[k][3])
-    return list_COORDINATES
+    final_coord = list_COORDINATES[-1]
+    return list_COORDINATES, total_time_job, final_coord
+
+def dict_GAUSSIAM(guassian_opt_nohup):
+    STEP = {}  # один большой словарь, где содержатся все ионные шаги
+    step_range = []
+    list_energy= energy_GAUSSIAN(guassian_opt_nohup)[0]
+    final_energy = energy_GAUSSIAN(guassian_opt_nohup)[1]
+    list_coord = coords_GAUSSIAN(guassian_opt_nohup)[0]
+    total_time = coords_GAUSSIAN(guassian_opt_nohup)[1]
+    print (len(list_energy), len(list_coord))
+    for i in range(len(list_energy)):
+        step_range.append('STEP' + str(i))
+    for key, value_1, value_2 in zip(step_range, list_energy, list_coord):
+        STEP[key] = {
+            "energy": value_1,
+            "coords": value_2
+        }
+    final_miny_dict = {
+        "final_energy": final_energy,
+        "total_time": total_time,
+        }
+    STEP['final_data'] = final_miny_dict
+    print(STEP)
 
 
 def main(file_path: str):
-    coords_GAUSSIAN(file_path)
-    #energy_ORCA(file_path)
+    dict_GAUSSIAM(file_path)
+    #coords_GAUSSIAN(file_path)
+    #dict_ORCA(file_path)
     # if 'orca' in file_path:
     #     dict_coord_and_energy = dict_ORCA(file_path)
     #     #print(dict_coord_and_energy)
